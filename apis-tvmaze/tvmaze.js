34fdb7b2fb70cbe3df695e38e67d491b57370261
Trevor <you@youremail.com>
2022-10-05 13:05:55 -0700

    added docStrings

--- apis-tvmaze/tvmaze.js
+++ apis-tvmaze/tvmaze.js
@@ -12,19 +12,19 @@ const TVMAZE_API = 'http://api.tvmaze.com/search/shows';
  *    (if no image URL given by API, put in a default image URL)
  */
 
-//function for retrieving best fit image
-// check if images exists
-// return best image size
 
+/** Function retrieves a show's poster image.
+ * Takes in a show object, returns a URL for the original image OR a boilerplate
+ * 'missing image' jpeg.
+ */
 function getImage(show) {
 
   const imageLocation = show.show.image.original;
-  const noImage = 'https://tinyurl.com/tv-missing';
+  const missingImage = 'https://tinyurl.com/tv-missing';
 
-  return (imageLocation ? imageLocation : noImage)
+  return (imageLocation ? imageLocation : missingImage)
 
 }
-
 
 
 
@@ -34,35 +34,12 @@ async function getShowsByTerm(searchTerm) {
   const response = await axios.get(TVMAZE_API,
     { params: { q: searchTerm } });
 
-  // const image = getImage(response);
-
-  // return an array of all of the shows
+  // returns an array of all of the shows
   return response.data;
 
 
 
-  // return [
-  //   {
-  //     id: 1767,
-  //     name: "The Bletchley Circle",
-  //     summary:
-  //       `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
-  //          women with extraordinary skills that helped to end World War II.</p>
-  //        <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
-  //          normal lives, modestly setting aside the part they played in
-  //          producing crucial intelligence, which helped the Allies to victory
-  //          and shortened the war. When Susan discovers a hidden code behind an
-  //          unsolved murder she is met by skepticism from the police. She
-  //          quickly realises she can only begin to crack the murders and bring
-  //          the culprit to justice with her former friends.</p>`,
-  //     image:
-  //         "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-  //   }
-  // ]
-}
-
-
-/** Given list of shows, create markup for each and to DOM */
+/** Given list of shows, create markup for each and append to DOM */
 
 function populateShows(shows) {
   $showsList.empty();
